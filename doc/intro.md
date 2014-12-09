ddebb7eb241641ac2ec1cccc735f1e55fda10a58
Milt Reder <milt@anestuary.com>
2014-12-09 16:39:26 -0500

    Let's get this party started

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hoser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject hoser "0.1.0-SNAPSHOT"
+  :description "Twitter Firehose -> xAPI"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+                 [twitter-api "0.7.7"]])
