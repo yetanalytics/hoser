d31e16b07cd7f24e877cf320f8d676deecdc95c7
Jason Lewis <jason@decomplecting.org>
2014-12-10 15:51:52 -0500

    Props work, client in progress

--- src/hoser/xapi/client.clj
+++ src/hoser/xapi/client.clj
@@ -6,3 +6,21 @@
 
 (def lrs-props
   (load-props "hoser.properties"))
+
+(def endpoint
+  (str (:lrs-uri lrs-props) (:path lrs-props)))
+
+(def options
+  {:timeout 200
+   :basic-auth [(:api-key lrs-props) (:api-secret lrs-props)]
+   :user-agent "Hoser"
+   :headers {"Accept" "application/json"
+             "Content-Type" "application/json"
+             "X-Experience-API-Version" "1.0.3"}})
+
+(defn send-stmt [stmt]
+  (http/post endpoint (merge options {:body stmt})
+             (fn [{:keys [status headers body error]}]
+               (if error
+                 (println "Failed, exception is " error)
+                 (println "Async HTTP POST: " status)))))
