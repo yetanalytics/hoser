ac2e85eb86839a8707ded32cbc93ccec0385a1a2
Jason Lewis <jason@decomplecting.org>
2014-12-10 16:02:28 -0500

    Sending works - fixed httpkit require

--- src/hoser/xapi/client.clj
+++ src/hoser/xapi/client.clj
@@ -1,6 +1,6 @@
 (ns hoser.xapi.client
   (:require [cheshire.core :as c]
-            [org.http-kit.client :as http]
+            [org.httpkit.client :as http]
             [hoser.util :refer [load-props]]))
 
 
