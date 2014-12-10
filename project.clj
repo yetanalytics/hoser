f8aa0bef40eeb10205f5be530bd5e22a16096bd8
Milt Reder <milt@anestuary.com>
2014-12-10 13:41:05 -0500

    add tentacles, change proj description

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject hoser "0.1.0-SNAPSHOT"
-  :description "Twitter Firehose -> xAPI"
+  :description "Github Events -> xAPI"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
-                 [twitter-api "0.7.7"]
+                 [tentacles "0.2.5"]
                  [cheshire "5.3.1"]
                  [http-kit "2.1.16"]])
