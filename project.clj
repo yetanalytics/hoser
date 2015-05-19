3afb6a56e1c2e9d87102cb23654ec9d1885d5e08
Jason Lewis <jason@decomplecting.org>
2015-05-19 01:21:49 -0400

    Desc desc

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject hoser "0.1.0-SNAPSHOT"
-  :description "Some Firehose -> xAPI"
+  :description "A Firehose -> xAPI"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
